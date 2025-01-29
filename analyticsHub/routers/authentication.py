08a1f288ad9693f83912f12ffc29abad64a533ba
rauhanahmed <rauhaan.siddiqui@gmail.com>
2025-01-30 04:33:55 +0530

    UPDATE: email verification

--- analyticsHub/routers/authentication.py
+++ analyticsHub/routers/authentication.py
@@ -1,6 +1,7 @@
 from ..models.requestModels import SignUp, Login, LoginWithProvider, OnboardingDetails
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from fastapi import APIRouter, HTTPException, Depends
+from supabase.lib.client_options import ClientOptions
 from fastapi.responses import JSONResponse
 from ..utils.functions import verifyToken
 from supabase import create_client
@@ -15,7 +16,11 @@ router = APIRouter()
 security = HTTPBearer()
 client = create_client(
     supabase_url = os.environ["SUPABASE_URL"],
-    supabase_key = os.environ["SUPABASE_KEY"]
+    supabase_key = os.environ["SUPABASE_KEY"],
+    options=ClientOptions(
+        auto_refresh_token=False,
+        persist_session=False,
+    )
 )
 
 @router.post("/signUp")
@@ -23,17 +28,52 @@ async def signup(signupDetails: SignUp):
     try:
         passwordString = signupDetails.password + os.environ["SECRET_KEY"]
         hashedPassword = hashlib.md5(passwordString.encode("utf-8")).hexdigest()
-        allData = pd.DataFrame(client.table("Users").select("userId", "email", "password").execute().data, columns = ["userId", "email", "password"])
-        if signupDetails.email not in allData["email"]:
-            response = client.table(table_name = "Users").insert(
+        allUsers = list()
+        page = 1
+        while True:
+            response = client.auth.admin.list_users(page = page, per_page = 1000)
+            if response == []:
+                break
+            else:
+                allUsers.extend(response)
+                page += 1
+        allUsers = [x.email for x in allUsers]
+        if signupDetails.email not in allUsers:
+            response = client.auth.sign_up(
+                {"email": signupDetails.email, "password": hashedPassword}
+            )
+            client.table(table_name = "Users").insert(
                 {
                     "email": signupDetails.email,
                     "password": hashedPassword
                 }
             ).execute()
-            return JSONResponse(status_code = 200, content = {"status": "SUCCESS"})
+            return JSONResponse(status_code = 200, content = {"status": "SUCCESS", "signupId": response.user.id})
         else:
             return JSONResponse(status_code = 409, content = {"status": "ERROR", "errorDetail": "User Already Exists"})
+    except Exception as e:
+        raise HTTPException(status_code = 500, detail = f"Endpoint says: {e}")
+
+@router.get("/confirmMail/{signupId}")
+async def confirmMail(signupId: str):
+    try:
+        allUsers = list()
+        page = 1
+        while True:
+            response = client.auth.admin.list_users(page = page, per_page = 1000)
+            if response == []:
+                break
+            else:
+                allUsers.extend(response)
+                page += 1
+        email = list(filter(lambda x: True if x.id == signupId else False, allUsers))[0].email
+        response = client.auth.resend({
+        "type": "signup",
+        "email": email,
+        "options": {
+            "email_redirect_to": "https://localhost:3000"
+        }
+        })
     except Exception as e:
         raise HTTPException(status_code = 500, detail = f"Endpoint says: {e}")
 
@@ -42,10 +82,22 @@ async def login(loginDetails: Login):
     try:
         passwordString = loginDetails.password + os.environ["SECRET_KEY"]
         hashedPassword = hashlib.md5(passwordString.encode("utf-8")).hexdigest()
-        allData = pd.DataFrame(client.table("Users").select("userId", "email", "password", "onboarded").execute().data, columns = ["userId", "email", "password", "onboarded"])
-        if loginDetails.email not in allData["email"].unique():
+        allUsers = list()
+        page = 1
+        while True:
+            response = client.auth.admin.list_users(page = page, per_page = 1000)
+            if response == []:
+                break
+            else:
+                allUsers.extend(response)
+                page += 1
+        filteredResult = list(filter(lambda x: True if x.email == loginDetails.email else False, allUsers))
+        if filteredResult == []:
             return JSONResponse(status_code = 404, content = {"status": "ERROR", "errorDetail": "User not found"})
+        elif filteredResult[0].user_metadata.get("email_verified") == False:
+            return JSONResponse(status_code = 401, content = {"status": "ERROR", "errorDetail": "Email not verified"})
         else:  
+            allData = pd.DataFrame(client.table("Users").select("userId", "email", "password", "onboarded").execute().data, columns = ["userId", "email", "password", "onboarded"])
             dataSlice = allData[allData["email"] == loginDetails.email].iloc[0, :]
             if dataSlice["password"] != hashedPassword:
                 return JSONResponse(status_code = 401, content = {"status": "ERROR", "errorDetail": "Invalid email or password"})
