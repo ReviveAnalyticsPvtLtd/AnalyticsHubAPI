8e6bf3a29f069f89a4672f5dfe615b8f4fbd56b6
rauhanahmed <rauhaan.siddiqui@gmail.com>
2025-05-12 23:01:12 +0530

    UPDATE: panel charts

--- analyticsHub/pipelines/pipeline.py
+++ analyticsHub/pipelines/pipeline.py
@@ -63,7 +63,6 @@ class CompletePipeline:
                 response = replManager.manager[projectId].run(f"getDataForChart(projectId='{projectId}', chartType='{chartType}', xAxis='{xAxis}', yAxis='{yAxis}', aggregationMetric='{aggregationMetric}', tablesUsed={tablesUsed}, joinTypes={joinTypes}, blendOn={blendOn})")
             else:
                 response = replManager.manager[projectId].run(f"getDataForChart(projectId='{projectId}', chartType='{chartType}', xAxis='{xAxis}', yAxis='{yAxis}', aggregationMetric='{aggregationMetric}', tablesUsed='{dataSource}')")    
-            print("response", response)
             response = json.loads(response)
             return response
         except Exception as e:
