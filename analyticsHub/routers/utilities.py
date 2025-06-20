17c5ff975f76950f54f1727f608830296eb2a190
rauhanahmed <rauhaan.siddiqui@gmail.com>
2025-06-20 19:42:58 +0530

    DEBUG: websocket

--- analyticsHub/routers/utilities.py
+++ analyticsHub/routers/utilities.py
@@ -51,7 +51,7 @@ async def getTaskStatus(websocket: WebSocket):
         await websocket.close(code = status.WS_1008_POLICY_VIOLATION)
         return 
     await websocket.accept()
-    await websocket.send_json({"status": "task initialized"})
+    await websocket.send_json({"status": "handshake successful"})
     try:
         taskId = websocket.query_params.get("taskId")
         r = task.celeryApp.AsyncResult(taskId)
