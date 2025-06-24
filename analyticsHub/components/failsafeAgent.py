7e7d46f710f1c43e0c72bc9c755d4f68016fa308
rauhanahmed <rauhaan.siddiqui@gmail.com>
2025-06-24 16:18:06 +0530

    DEBUG: llama 3.3

--- analyticsHub/components/failsafeAgent.py
+++ analyticsHub/components/failsafeAgent.py
@@ -3,8 +3,8 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from ..utils.functions import readYaml, getConfig
 from ..utils.exceptions import CustomException
-# from langchain_cerebras import ChatCerebras
-from langchain_openai import ChatOpenAI
+from langchain_cerebras import ChatCerebras
+# from langchain_openai import ChatOpenAI
 from ..utils.logger import logger
 import os
 
@@ -19,17 +19,17 @@ class FailsafeCodeGenerator:
             logger.info("Constructing failsafe code generation chain.")
             promptTemplate = readYaml(self.yamlPath)["codeDebuggerAgentPrompt"]
             codeGeneratorPrompt = PromptTemplate.from_template(promptTemplate)
-            # llm = ChatCerebras(
-            #     model=self.config.get("FAILSAFECODEGENERATOR", "model"),
-            #     temperature=self.config.getfloat("FAILSAFECODEGENERATOR", "temperature")
-            # )
-            llm = ChatOpenAI(
-                openai_api_key = os.environ["OPENAI_API_KEY"],
-                openai_api_base = os.environ["OPENAI_API_BASE"],
-                model_name = self.config.get("FAILSAFECODEGENERATOR", "model"),
-                temperature = self.config.getfloat("FAILSAFECODEGENERATOR", "temperature"),
-                max_tokens = self.config.getint("FAILSAFECODEGENERATOR", "maxTokens")
+            llm = ChatCerebras(
+                model=self.config.get("FAILSAFECODEGENERATOR", "model"),
+                temperature=self.config.getfloat("FAILSAFECODEGENERATOR", "temperature")
             )
+            # llm = ChatOpenAI(
+            #     openai_api_key = os.environ["OPENAI_API_KEY"],
+            #     openai_api_base = os.environ["OPENAI_API_BASE"],
+            #     model_name = self.config.get("FAILSAFECODEGENERATOR", "model"),
+            #     temperature = self.config.getfloat("FAILSAFECODEGENERATOR", "temperature"),
+            #     max_tokens = self.config.getint("FAILSAFECODEGENERATOR", "maxTokens")
+            # )
             codeGeneratorParser = StrOutputParser()
             failsafeCodeGeneratorChain = RunnablePassthrough() | codeGeneratorPrompt | llm | codeGeneratorParser
             logger.info("failsafe code generation chain constructed successfully.")
